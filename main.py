ca68e881332de92f91b3d28ada995c3991ab2877
Marcus Oshodi <moshodi>
2023-04-21 08:49:22 -0400

    changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 import spacy 
-from spacy import displacy
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-
+class QueryInput(BaseModel):
+    query: str
 
 txt = "The tallest living man is 37-year-old Sultan Kosen, from Turkey, who is 8 feet, 2.8 inches, who set the record in 2009."
 
@@ -18,5 +18,8 @@ for ent in doc.ents:
     print(ent.text+':',ent.label_)
 print(nlp.pipe_names)
 
-# @app.get('/model/{text}')
-# def get_entities(text: )
+@app.post('/model')
+def get_entities(text: str):
+    doc = nlp(text)
+    entities = [f'{ent.text}->{ent.label_}' for ent in doc.ents]
+    return {'entities':entities}
